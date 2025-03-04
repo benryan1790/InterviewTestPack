96dbeeec6c0a7e6ea0274087efe0c1a0b91368c8
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2025-03-04 13:37:08 +0000

    Update test class name

--- src/test/java/InitialTest.java
+++ src/test/java/InitialTest.java
@@ -1,16 +1,14 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FirstSelenium {
+public class InitialTest {
 
     WebDriver driver;
 
