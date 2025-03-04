3982483e084c60c20bcd5c30777fd00e0b1c2549
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2025-03-04 11:54:52 +0000

    First commit

---
+++ src/test/java/FirstSelenium.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstSelenium {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("https://www.saucedemo.com/");
+
+        String title = driver.getTitle();
+        assertEquals("Swag Labs", title);
+
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+
+}
